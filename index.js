e7e2ddbd5f10fb453be4199bfbacdfd15ce23512
Zach Plata <plata.zach@gmail.com>
2017-10-31 15:30:47 -0500

    Add node setup and start work on node app (#1)

--- index.js
+++ index.js
@@ -1,45 +1,81 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const patient_hello_world_service = require('./services/patient-hello-world');
+const app = express();
+
+// This is necessary middleware to parse JSON into the incoming request body for POST requests
+app.use(bodyParser.json());
+
+app.use((request, response, next) => {
+  if (request.method !== 'GET' && request._contentType && request._contentType.match('json\\+fhir')){
+    request.body = request.body ? JSON.parse(request.body.toString()) : {}
+  }
+
+  next();
+});
+
 /**
- * Responds to any HTTP request that can provide a "message" field in the body.
- *
- * @param {!Object} req Cloud Function request context.
- * @param {!Object} res Cloud Function response context.
+ * Security Considerations:
+ * - CDS Services must implement CORS in order to be called from a web browser
  */
-exports['cds-services'] = function cdsServices(req, res) {
-  const patientHelloWorldService = {
-    id: "patient-hello-world",
-    name: "Patient hello world", // Remove on complete transition to CDS Hooks 1.0 Spec
-    title: "Patient hello world",
-    description: "Greet patient by name",
-    hook: "patient-view",
-    prefetch: {
-      patientToGreet: "Patient/{{Patient.id}}"
-    }
-  };
+app.use((request, response, next) => {
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  response.setHeader('Access-Control-Allow-Credentials', 'true');
+  response.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  response.setHeader('Access-Control-Expose-Headers', 'Origin, Accept, Content-Location, ' +
+    'Location, X-Requested-With');
 
-  const cmsPriceCheckService = {
-    name: "CMS Pricing Service", // Remove on complete transition to CDS Hooks 1.0 Spec
-    title: "CMS Pricing Service",
-    id: "cms-price-check",
-    description: "Estimate the price of a prescription based on historical pharmacy dispensing data",
-    hook: "medication-prescribe"
-  };
+  // Pass to next layer of middleware
+  next();
+});
 
-  const pediatricDoseCheck = {
-    hook: "medication-prescribe",
-    name: "Random grab-bag of mock services", // Remove on complete transition to CDS Hooks 1.0 Spec
-    title: "Random grab-bag of mock services",
-    description: "Generate a bunch of cards for various reasons",
-    id: "pediatric-dose-check",
-    prefetch:{
-      patient: "Patient/{{Patient.id}}"
-    }
-  };
-
-  return res.json({
-    services: [
-      patientHelloWorldService,
-      cmsPriceCheckService,
-      pediatricDoseCheck
-    ]
-  })
+// Keys of this object are CDS Service ID's
+const services = {
+  'patient-hello-world': patient_hello_world_service
 };
+
+const isValidCdsService = (id) => {
+  return id in services;
+};
+
+const isValidJson = (obj) => {
+  try {
+    JSON.stringify(obj);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
+
+const discoveryDefinitions = Object.keys(services).map((serviceId) => {
+  return services[serviceId].definition
+});
+
+app.get('/cds-services', (request, response) => {
+  const discoveryEndpointServices = {
+    services: discoveryDefinitions
+  };
+  response.send(JSON.stringify(discoveryEndpointServices, null, 2));
+});
+
+app.post('/cds-services/:serviceId', (request, response) => {
+  const serviceEndpoint = request.params.serviceId;
+
+  // Invalid CDS Service endpoint
+  if (!isValidCdsService(serviceEndpoint)) {
+    response.status(404);
+  }
+
+  // Bad request
+  if (!isValidJson(request.body)) {
+    response.status(400);
+  }
+
+  var cdsService = services[serviceEndpoint];
+  cdsService.serviceResponse(request.body, (err, serviceResponse) => {
+    response.send(JSON.stringify(serviceResponse, null, 2));
+  });
+});
+
+app.listen((process.env.PORT || 3000));
